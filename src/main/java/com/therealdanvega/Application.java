80cffb2c886a004903719e29d8797c366d0774ca
andreeacodreanu <andrex_97@yahoo.com>
2019-12-12 14:24:09 +0200

    primul commit din intellij

--- src/main/java/com/therealdanvega/Application.java
+++ src/main/java/com/therealdanvega/Application.java
@@ -3,6 +3,8 @@ package com.therealdanvega;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//test commit
 @SpringBootApplication
 public class Application {
 	
