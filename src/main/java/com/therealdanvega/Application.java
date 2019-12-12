d50765e68833420320477af52c77405e6112c17d
Andreea <andrex_97@yahoo.com>
2019-12-12 15:58:52 +0200

    commit

--- src/main/java/com/therealdanvega/Application.java
+++ src/main/java/com/therealdanvega/Application.java
@@ -3,12 +3,6 @@ package com.therealdanvega;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-//test commit
-//test commit 2
-//test backwards
-//test commit 3
-//Test commit Andreea
 @SpringBootApplication
 public class Application {
 	
