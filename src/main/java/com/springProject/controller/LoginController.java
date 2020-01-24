54d3a30c4d166dbefe21da24ff1bdcb8dc166b8e
Andreea <andrex_97@yahoo.com>
2020-01-24 19:08:28 +0200

    added user home page and changed routes

--- src/main/java/com/springProject/controller/LoginController.java
+++ src/main/java/com/springProject/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.springProject.controller;
 
 import javax.validation.Valid;
 
+import com.springProject.model.Role;
 import com.springProject.model.User;
 import com.springProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Set;
 
 @Controller
 public class LoginController {
@@ -25,7 +28,6 @@ public class LoginController {
         modelAndView.setViewName("login");
         return modelAndView;
     }
-
 
     @RequestMapping(value="/registration", method = RequestMethod.GET)
     public ModelAndView registration(){
@@ -52,21 +54,29 @@ public class LoginController {
             modelAndView.addObject("successMessage", "User has been registered successfully");
             modelAndView.addObject("user", new User());
             modelAndView.setViewName("registration");
-
         }
         return modelAndView;
     }
 
-    @RequestMapping(value="/admin/home", method = RequestMethod.GET)
+    @RequestMapping(value="/home", method = RequestMethod.GET)
     public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByEmail(auth.getName());
-        modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
-        modelAndView.setViewName("admin/home");
-        return modelAndView;
+        Set<Role> roles = user.getRoles();
+        Role admin = new Role(1,"ADMIN");
+        if (roles.contains(admin)) {
+            modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+            modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+            modelAndView.setViewName("admin/home");
+            return modelAndView;
+        }
+        else {
+            modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+            modelAndView.addObject("userMessage","Content Available for Users");
+            modelAndView.setViewName("user/home");
+            return modelAndView;
+        }
     }
-
 
 }
